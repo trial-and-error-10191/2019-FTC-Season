e391fa9617b187586fa876035ebb1906e1606ddf
AirBorngaming-x <56490870+AirBorngaming-x@users.noreply.github.com>
2020-01-04 17:48:05 -0600

    Add files via upload

---
+++ Jack/StonemanipulatorHardware.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+
+public class StonemanipulatorHardware {
+    DcMotor iift1;
+    DcMotor intake1;
+    DcMotor intake2;
+    DcMotor extension;
+    Servo trapDoor;
+    TouchSensor loadingBaySensor;
+    ColorSensor stoneDetector;
+    int EXTENSION_COUNTS;
+
+
+
+
+
+
+
+}
+
