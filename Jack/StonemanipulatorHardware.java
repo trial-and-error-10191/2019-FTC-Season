c9589bed952d07dec02ba9c78f068cba15402c0f
AirBorngaming-x <56490870+AirBorngaming-x@users.noreply.github.com>
2020-01-09 20:03:48 -0600

    Add files via upload

--- Jack/StonemanipulatorHardware.java
+++ Jack/StonemanipulatorHardware.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 
 public class StonemanipulatorHardware {
-    DcMotor iift1;
+    DcMotor lift1;
     DcMotor intake1;
     DcMotor intake2;
     DcMotor extension;
@@ -22,8 +22,51 @@ public class StonemanipulatorHardware {
     int EXTENSION_COUNTS;
 
 
+    public void setIntakePower(double p) {
+        intake1.setPower(p);
+        intake2.setPower(p);
 
 
+    }
+
+    public boolean isloaded(boolean loaded){
+
+
+        return loaded;
+    }
+
+
+
+    void setLiftPower(double p) {
+        lift1.setPower(p);
+
+    }
+
+
+    void moveLiftToLevel(double p,int level) {
+
+
+
+    }
+
+    void setOuttakePower(double position) {
+        trapDoor.setPosition(position);
+
+
+    }
+
+    void extend(double p) {
+
+
+    }
+
+
+    void toggleTrapDoor() {
+
+
+
+
+    }
 
 
 
